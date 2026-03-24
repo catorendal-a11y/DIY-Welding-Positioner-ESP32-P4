e508e1fc81f6510373f3b9c7d28ee105b9f410f0
Rendalsniken <Catorendal@gmail.com>
2026-03-24 17:02:41 +0100

    fix: exclude all reference BSP folders from git

--- ESP32-P4-WIFI6-Touch-LCD-4.3-main/examples/esp-idf/11_esp_brookesia_phone/components/esp32_p4_wifi6_touch_lcd_4_3/include/bsp/config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/**************************************************************************************************
- * BSP configuration
- **************************************************************************************************/
-// By default, this BSP is shipped with LVGL graphical library. Enabling this option will exclude it.
-// If you want to use BSP without LVGL, select BSP version with 'noglib' suffix.
